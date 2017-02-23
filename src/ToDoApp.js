c73830caff1e9a5a185635cc563d539df51b2cf7
CJ Cenizal <cj@cenizal.com>
2017-02-22 16:37:38 -0800

    Initial commit. - Add images to README.

---
+++ src/ToDoApp.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class ToDoApp extends Component {
+  render() {
+    return (
+      <div>Hello world</div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ToDoApp } from './ToDoApp';
+import './index.css';
+
+ReactDOM.render(
+  <ToDoApp />,
+  document.getElementById('root')
+);
