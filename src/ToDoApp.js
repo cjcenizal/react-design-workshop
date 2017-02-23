91c1d75a276ca599ede1e1828eb525433eb05915
CJ Cenizal <cj@cenizal.com>
2017-02-22 21:00:05 -0800

    Add all CSS and demo markup.

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -8,35 +8,63 @@ export class ToDoApp extends Component {
           To-dos
         </h1>
 
+        <label className="label">
+          Search
+        </label>
+
+        <input type="text" className="textInput" />
+
+        <button className="sortButton">
+          Sort ascending
+        </button>
+
         <ol className="toDoList">
           <li className="toDoListItem">
             <div className="toDoListItem__body">
               Build React app
             </div>
 
-            <button className="toDoListItem__delete">
-              Delete
-            </button>
+            <div className="toDoListItem__actions">
+              <button className="toDoListItem__button">
+                Delete
+              </button>
+
+              <button className="toDoListItem__button">
+                Edit
+              </button>
+            </div>
           </li>
 
           <li className="toDoListItem">
             <div className="toDoListItem__body">
-              ???
+              <input
+                type="text"
+                className="textInput textInput--inline"
+                value="???"
+              />
             </div>
 
-            <button className="toDoListItem__delete">
-              Delete
-            </button>
+            <div className="toDoListItem__actions">
+              <button className="toDoListItem__button">
+                Save
+              </button>
+            </div>
           </li>
 
-          <li className="toDoListItem">
+          <li className="toDoListItem toDoListItem--critical">
             <div className="toDoListItem__body">
               Profit!
             </div>
 
-            <button className="toDoListItem__delete">
-              Delete
-            </button>
+            <div className="toDoListItem__actions">
+              <button className="toDoListItem__button">
+                Delete
+              </button>
+
+              <button className="toDoListItem__button">
+                Edit
+              </button>
+            </div>
           </li>
         </ol>
 
@@ -47,6 +75,13 @@ export class ToDoApp extends Component {
         </label>
 
         <input type="text" className="textInput" />
+
+        <label className="checkBoxLabel">
+          <input type="checkbox" className="checkBox" />
+          <span className="checkBoxLabel__text">
+            This is critical
+          </span>
+        </label>
 
         <button className="button">
           Create To-Do
