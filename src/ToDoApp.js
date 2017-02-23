bf6726976c72c3360d2bfae086184e6d4a624627
CJ Cenizal <cj@cenizal.com>
2017-02-22 16:37:38 -0800

    Create workshop starting point.

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 export class ToDoApp extends Component {
+  constructor() {
+    super();
+  }
+
   render() {
     return (
       <div>Hello world</div>
